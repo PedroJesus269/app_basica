877bb094117af48a3e95804017f1872e5689b96e
PedroJesus269 <pedroahuanlla7@gmail.com>
2024-11-27 20:22:33 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# streamlit_app.py
+
+import streamlit as st
+
+# Título de la app
+st.title('Mi Primera Aplicación con Streamlit')
+
+# Descripción
+st.write('¡Hola! Esta es una aplicación básica de Streamlit.')
+
+# Caja de texto para entrada del usuario
+nombre = st.text_input('¿Cómo te llamas?')
+
+# Mostrar el saludo
+if nombre:
+    st.write(f'¡Hola, {nombre}! Encantado de conocerte.')
